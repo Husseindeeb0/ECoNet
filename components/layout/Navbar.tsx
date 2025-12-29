877d6fdad9066a4df673c404a319cc4284a1f18d
Hussein <deebh776@gmail.com>
2025-12-30 00:55:06 +0200

    Change Logo

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -10,7 +10,6 @@ import {
   UserCircle,
   MessageSquare,
   LayoutDashboard,
-  Users,
   ClipboardCheck,
 } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
@@ -116,7 +115,7 @@ export const Navbar = () => {
               href="/"
               className="flex items-center space-x-2 text-xl font-bold text-white transition-all duration-300"
             >
-              <img src="/logo.png" alt="ECoNet Logo" className="w-40" />
+              <img src="/logo.png" alt="ECoNet Logo" className="w-44 h-32" />
             </Link>
           </div>
 
